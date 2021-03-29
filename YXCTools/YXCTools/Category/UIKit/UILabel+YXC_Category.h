61a66c53b0e42f755a0462b7df933fbd9faa4598
guoguangtao <Jude_guo@163.com>
2021-03-29 19:21:41 +0800

    添加 UILabel  分类

---
+++ YXCTools/YXCTools/Category/UIKit/UILabel+YXC_Category.h
@@ -0,0 +1,17 @@
+//
+//  UILabel+YXC_Category.h
+//  YXCTools
+//
+//  Created by GGT on 2021/3/29.
+//  Copyright © 2021 GGT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (YXC_Category)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YXCTools/YXCTools/Category/UIKit/UILabel+YXC_Category.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+YXC_Category.m
+//  YXCTools
+//
+//  Created by GGT on 2021/3/29.
+//  Copyright © 2021 GGT. All rights reserved.
+//
+
+#import "UILabel+YXC_Category.h"
+
+@implementation UILabel (YXC_Category)
+
+@end
