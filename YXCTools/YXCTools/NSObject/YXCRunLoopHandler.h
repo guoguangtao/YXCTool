db2d9487257c41807f612493ea155ae60bc8ed95
guoguangtao <Jude_guo@163.com>
2021-07-27 10:02:13 +0800

    调整目录结构

---
+++ YXCTools/YXCTools/NSObject/YXCRunLoopHandler.h
@@ -0,0 +1,18 @@
+//
+//  YXCRunLoopHandler.h
+//  YXCTools
+//
+//  Created by GGT on 2021/7/27.
+//  Copyright © 2021 GGT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXCRunLoopHandler : NSObject
+
+#pragma mark - Property
+
+
+#pragma mark - Method
+
+@end
---
+++ YXCTools/YXCTools/NSObject/YXCRunLoopHandler.m
@@ -0,0 +1,43 @@
+//
+//  YXCRunLoopHandler.m
+//  YXCTools
+//
+//  Created by GGT on 2021/7/27.
+//  Copyright © 2021 GGT. All rights reserved.
+//
+
+#import "YXCRunLoopHandler.h"
+
+@interface YXCRunLoopHandler ()
+
+
+
+@end
+
+@implementation YXCRunLoopHandler
+
+#pragma mark - Lifecycle
+
+- (void)dealloc {
+    
+    NSLog(@"%s", __func__);
+}
+
+
+#pragma mark - Custom Accessors (Setter 方法)
+
+
+#pragma mark - Public
+
+
+#pragma mark - Private
+
+
+#pragma mark - Protocol
+
+
+#pragma mark - 懒加载
+
+
+
+@end
