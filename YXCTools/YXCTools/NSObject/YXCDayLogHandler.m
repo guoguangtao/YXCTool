2934916c6e38ccd15e01ef37fab22db4f8525855
guoguangtao <Jude_guo@163.com>
2021-11-17 19:43:57 +0800

    日志导出

--- YXCTools/YXCTools/NSObject/YXCDayLogHandler.m
+++ YXCTools/YXCTools/NSObject/YXCDayLogHandler.m
@@ -26,13 +26,26 @@
 
 + (void)redirectLEBLogToDocumentFolder {
     NSString *documentDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-    
+    documentDirectory = [documentDirectory stringByAppendingPathComponent:@"DayLogFile"];
+    NSFileManager *defaultManager = [NSFileManager defaultManager];
+    BOOL isDirectory = NO;
+    BOOL existed = [defaultManager fileExistsAtPath:documentDirectory isDirectory:&isDirectory];
+    if (!(isDirectory == YES && existed == YES)) {
+        // 创建文件夹
+        [defaultManager createDirectoryAtPath:documentDirectory withIntermediateDirectories:YES attributes:nil error:nil];
+    }
     NSDateFormatter *dateformat = [[NSDateFormatter  alloc]init];
-    [dateformat setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
-    NSString *fileName = [NSString stringWithFormat:@"Log-%@.txt",[dateformat stringFromDate:[NSDate date]]];
+    [dateformat setDateFormat:@"YYYY_MM_dd"];
+    documentDirectory = [documentDirectory stringByAppendingPathComponent:[dateformat stringFromDate:[NSDate date]]];
+    isDirectory = NO;
+    existed = [defaultManager fileExistsAtPath:documentDirectory isDirectory:&isDirectory];
+    if (!(isDirectory == YES && existed == YES)) {
+        [defaultManager createDirectoryAtPath:documentDirectory withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    [dateformat setDateFormat:@"HH_mm_ss"];
+    NSString *fileName = [NSString stringWithFormat:@"%@.txt",[dateformat stringFromDate:[NSDate date]]];
     NSString *logFilePath = [documentDirectory stringByAppendingPathComponent:fileName];
     // 先删除已经存在的文件
-    NSFileManager *defaultManager = [NSFileManager defaultManager];
     [defaultManager removeItemAtPath:logFilePath error:nil];
     // 将log输入到文件
     freopen([logFilePath cStringUsingEncoding:NSASCIIStringEncoding], "a+", stdout);
