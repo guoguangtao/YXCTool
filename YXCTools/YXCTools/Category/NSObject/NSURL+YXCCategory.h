3bf826ab2930fa60dcb6532ba32d79ce18bf28df
guoguangtao <Jude_guo@163.com>
2023-05-11 21:35:23 +0800

    lebo_ggt_master:NSURL 分类修改
    起因: 无
    结果: 无
    单号: 无
    
    Signed-off-by: ggt <guangtao_guo@lebo.cn>

--- YXCTools/YXCTools/Category/NSObject/NSURL+YXCCategory.h
+++ YXCTools/YXCTools/Category/NSObject/NSURL+YXCCategory.h
@@ -12,25 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSURL (YXCCategory)
 
 /// URL 解析对象
-@property (nonatomic, nullable, strong, readonly) NSURLComponents *components;
-/// URL 链接方案
-@property (nonatomic, nullable, copy, readonly) NSString *scheme;
-/// URL 用户名
-@property (nonatomic, nullable, copy, readonly) NSString *user;
-/// URL 密码
-@property (nonatomic, nullable, copy, readonly) NSString *password;
-/// URL 域名
-@property (nonatomic, nullable, copy, readonly) NSString *host;
-/// URL 端口号
-@property (nonatomic, nullable, strong, readonly) NSNumber *port;
-/// URL 路径
-@property (nonatomic, nullable, copy, readonly) NSString *path;
-/// URL 参数字符串
-@property (nonatomic, nullable, copy, readonly) NSString *query;
-/// URL 参数数组
-@property (nonatomic, nullable, strong, readonly) NSArray<NSURLQueryItem *> *queryItems;
+@property (nonatomic, nullable, strong, readonly) NSURLComponents *yxc_components;
 /// URL 参数字典
-@property (nonatomic, nullable, strong, readonly) NSDictionary *queryItemsDictionary;
+@property (nonatomic, nullable, strong, readonly) NSDictionary *yxc_queryItemsDictionary;
 
 @end
 
--- YXCTools/YXCTools/Category/NSObject/NSURL+YXCCategory.m
+++ YXCTools/YXCTools/Category/NSObject/NSURL+YXCCategory.m
@@ -10,66 +10,26 @@
 
 @implementation NSURL (YXCCategory)
 
-- (NSString *)scheme {
-
-    return self.components.scheme;
-}
-
-- (NSString *)user {
-
-    return self.components.user;
-}
-
-- (NSString *)password {
-
-    return self.components.password;
-}
-
-- (NSString *)host {
-
-    return self.components.host;
-}
-
-- (NSNumber *)port {
-
-    return self.components.port;
-}
-
-- (NSString *)path {
-
-    return self.components.path;
-}
-
-- (NSString *)query {
-
-    return self.components.query;
-}
-
-- (NSArray<NSURLQueryItem *> *)queryItems {
-
-    return self.components.queryItems;
-}
-
-- (NSDictionary *)queryItemsDictionary {
+- (NSDictionary *)yxc_queryItemsDictionary {
 
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-    
-    [self.queryItems enumerateObjectsUsingBlock:^(NSURLQueryItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+
+    [self.yxc_components.queryItems enumerateObjectsUsingBlock:^(NSURLQueryItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         [dictionary setValue:obj.value forKey:obj.name];
     }];
-    
+
     return dictionary;
 }
 
-- (NSURLComponents *)components {
-    
-    NSURLComponents *components = objc_getAssociatedObject(self, @selector(components));
-    
+- (NSURLComponents *)yxc_components {
+
+    NSURLComponents *components = objc_getAssociatedObject(self, @selector(yxc_components));
+
     if (components == nil) {
         components = [[NSURLComponents alloc] initWithString:self.absoluteString];
-        objc_setAssociatedObject(self, @selector(components), components, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(self, @selector(yxc_components), components, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
-    
+
     return components;
 }
 
